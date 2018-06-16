1373f486f3ca669a1e8294573cc70543ff6f1a70
Arjun-Ani <arjunani19135@gmail.com>
2018-06-16 10:04:17 +0530

    arjun2

--- exp17.py
+++ exp17.py
@@ -1,5 +1,13 @@
 a=raw_input("Enter the Number of transactions\n")
 b=[raw_input("Enter the transaction\n") for i in range(int(a))]
 c=[i.split(' ') for i in b]
-print dict(c)
+money=0
+d=dict(c)
+for i in d.iteritems():
+    if i[0]=="D":
+        money=money+int(i[1])
+    if i[0]=="W":
+        money=money-int(i[1])
+
+print "Remaining Balance=%s" %money
 
